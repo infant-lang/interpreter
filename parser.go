49f9eeb80b181c4a05f690b572de75dc53e8a4bb
tharunoptimus <tharunoptimus@outlook.com>
2021-12-31 19:37:11 +0530

    Separated a function into two for simpler implementation

--- parser.go
+++ parser.go
@@ -1,11 +1,33 @@
 package main
 
-import (
-	"fmt"
-)
-
 func parse(line string, lineNumber int, p int, m int) (int, int) {
 	tokens := lex(line, lineNumber)
+	p, m = parser(tokens, line, lineNumber, p, m)
+
+	return p, m
+}
+
+/*
+Function that gets the tokens of the line that is currently being parsed
+	For Conditionals and Looping, the function will call itself recursively until it
+reaches the end of the line finding the statement in the current condition
+
+Parameters:
+	- tokens: []token - the tokens of the line currently being parsed
+	- line: string - the line currently being parsed
+	- lineNumber: int - the line number of the line currently being parsed
+	- p: int - the current position of the pointer
+	- m: int - the current memory value
+
+Return Values:
+	- p: int - the new position of the pointer
+	- m: int - the new memory value
+*/
+func parser(tokens []token, line string, lineNumber int, p int, m int) (int, int) {
+
+	if len(tokens) == 0 {
+		return p, m
+	}
 
 	if tokens[0].tokenType == "ACTION" {
 		actionTokens := checkAction(tokens, line, lineNumber)
