2d4e156b622f09e03047c6570dd1375071fb3838
tharunoptimus <tharunoptimus@outlook.com>
2022-01-14 14:27:03 +0530

    Improved the error reporting of lexer

--- lexer.go
+++ lexer.go
@@ -104,38 +104,38 @@ func lex(line string, lineNumber int) []token {
 					if line[i+3] == 'r' {
 						tokens = append(tokens, token{"CHAR", "char"}) // char
 						i = i + 3
-					}
-				}
-			}
+					} else { printError(line, lineNumber, i+3) }
+				} else { printError(line, lineNumber, i+2) }
+			} else { printError(line, lineNumber, i+1) }
 		} else if line[i] == 'f' {
 			if line[i+1] == 'o' {
 				if line[i+2] == 'r' {
 					tokens = append(tokens, token{"LOOP", "for"}) // for
 					i = i + 2
-				}
-			}
+				} else { printError(line, lineNumber, i+2) }
+			} else { printError(line, lineNumber, i+1)}
 		} else if line[i] == 'i' {
 			if line[i+1] == 'f' {
 				tokens = append(tokens, token{"CONDITION", "if"}) // if
 				i = i + 1
-			}
+			} else { printError(line, lineNumber, i+1) }
 		} else if line[i] == 'l' {
 			if line[i+1] == 'e' {
 				if line[i+2] == 'f' {
 					if line[i+3] == 't' {
 						tokens = append(tokens, token{"DIRECTION", "left"}) // left
 						i = i + 3
-					}
-				}
-			}
+					} else { printError(line, lineNumber, i+3) }
+				} else { printError(line, lineNumber, i+2) }
+			} else { printError(line, lineNumber, i+1) }
 		} else if line[i] == 'm' {
 			if line[i+1] == 'o' {
 				if line[i+2] == 'v' {
 					if line[i+3] == 'e' {
 						tokens = append(tokens, token{"ACTION", "move"}) // move
 						i = i + 3
-					}
-				}
+					} else { printError(line, lineNumber, i+3) }
+				} else { printError(line, lineNumber, i+2) }
 			} else if line[i+1] == 'e' {
 				if line[i+2] == 'm' {
 					if line[i+3] == 'o' {
@@ -143,18 +143,18 @@ func lex(line string, lineNumber int) []token {
 							if line[i+5] == 'y' {
 								tokens = append(tokens, token{"MEMORY", "memory"}) // memory
 								i = i + 5
-							}
-						}
-					}
-				}
-			}
+							} else { printError(line, lineNumber, i+5) }
+						} else { printError(line, lineNumber, i+4) }
+					} else { printError(line, lineNumber, i+3) }
+				} else { printError(line, lineNumber, i+2) }
+			} else { printError(line, lineNumber, i+1) }
 		} else if line[i] == 'n' {
 			if line[i+1] == 'e' {
 				if line[i+2] == 'w' {
 					tokens = append(tokens, token{"SPECIAL", "new"}) // new
 					i = i + 2
-				}
-			}
+				} else { printError(line, lineNumber, i+2) }
+			} else { printError(line, lineNumber, i+1) }
 		} else if line[i] == 'p' {
 			if line[i+1] == 'r' {
 				if line[i+2] == 'i' {
@@ -162,9 +162,9 @@ func lex(line string, lineNumber int) []token {
 						if line[i+4] == 't' {
 							tokens = append(tokens, token{"PRINT", "print"}) // print
 							i = i + 4
-						}
-					}
-				}
+						} else { printError(line, lineNumber, i+4) }
+					} else { printError(line, lineNumber, i+3) }
+				} else { printError(line, lineNumber, i+2) }
 			} else if line[i+1] == 'o' {
 				if line[i+2] == 'i' {
 					if line[i+3] == 'n' {
@@ -173,12 +173,12 @@ func lex(line string, lineNumber int) []token {
 								if line[i+6] == 'r' {
 									tokens = append(tokens, token{"POINTER", "pointer"}) // pointer
 									i = i + 6
-								}
-							}
-						}
-					}
-				}
-			}
+								} else { printError(line, lineNumber, i+6) }
+							} else { printError(line, lineNumber, i+5) }
+						} else { printError(line, lineNumber, i+4) }
+					} else { printError(line, lineNumber, i+3) }
+				} else { printError(line, lineNumber, i+2) }
+			} else { printError(line, lineNumber, i+1) }
 		} else if line[i] == 'r' {
 			if line[i+1] == 'i' {
 				if line[i+2] == 'g' {
@@ -186,10 +186,10 @@ func lex(line string, lineNumber int) []token {
 						if line[i+4] == 't' {
 							tokens = append(tokens, token{"DIRECTION", "right"}) // right
 							i = i + 4
-						}
-					}
-				}
-			}
+						} else { printError(line, lineNumber, i+4) }
+					} else { printError(line, lineNumber, i+3) }
+				} else { printError(line, lineNumber, i+2) }
+			} else { printError(line, lineNumber, i+1) }
 		} else if line[i] == 's' { 
 			if line[i+1] == 'p' {
 				if line[i+2] == 'a' {
@@ -197,18 +197,18 @@ func lex(line string, lineNumber int) []token {
 						if line[i+4] == 'e' {
 							tokens = append(tokens, token{"SPECIAL", "space"}) // space
 							i = i + 4
-						}
-					}
-				}
-			}
+						} else { printError(line, lineNumber, i+4) }
+					} else { printError(line, lineNumber, i+3) }
+				} else { printError(line, lineNumber, i+2) }
+			} else { printError(line, lineNumber, i+1) }
 			
 		} else if line[i] == 't' {
 			if line[i+1] == 'a' {
 				if line[i+2] == 'b' {
 					tokens = append(tokens, token{"SPECIAL", "tab"}) // tab
 					i = i + 2
-				}
-			}
+				} else { printError(line, lineNumber, i+2) }
+			} else { printError(line, lineNumber, i+1) }
 		} else if line[i] == '0' || line[i] == '1' || line[i] == '2' || line[i] == '3' ||
 			line[i] == '4' || line[i] == '5' || line[i] == '6' || line[i] == '7' ||
 			line[i] == '8' || line[i] == '9' {
